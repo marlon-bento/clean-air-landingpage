ff57ad2186f39e47c2766a401d44b0cd7c637789
Marlon Bento <50771135+marlon-bento@users.noreply.github.com>
2024-11-25 11:35:24 -0300

    Update Home.vue

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,13 @@
 <template>
-   
+   <div>
       <Calltoaction></Calltoaction>
       <SobreNos></SobreNos>
       <PorQueEscolher></PorQueEscolher>
       <Patrocinio></Patrocinio>
       <NossaVisao></NossaVisao>
       <Beneficios></Beneficios>
+   </div>
+      
      
    
 
@@ -18,3 +20,10 @@ import Patrocinio from "./Patrocinio.vue";
 import NossaVisao from "./NossaVisao.vue";
 import Beneficios from "./Beneficios.vue";
 </script>
+
+<style scoped>
+div{
+   margin-top:100px;
+}
+
+</style>
